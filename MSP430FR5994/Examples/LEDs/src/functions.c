8a00d18aa0a6d0b300175098ca507a6cf971d0dc
Manuel Caballero <aqueronteblog@gmail.com>
2018-09-19 20:33:38 +0100

    MSP430FR5994 LEDs Example. Functions files were added

---
+++ MSP430FR5994/Examples/LEDs/src/functions.c
@@ -0,0 +1,17 @@
+/**
+ * @brief       functions.c
+ * @details     Functions.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        19/September/2018
+ * @version     19/September/2018   The ORIGIN
+ * @warning     N/A
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+
+#include "functions.h"
+
+
