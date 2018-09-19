ba5f1a2289d14c767bb73c57e3f1271f6d4a0a2f
Manuel Caballero <aqueronteblog@gmail.com>
2018-09-19 20:33:51 +0100

    MSP430FR5994 LEDs Example. Variables files were added

---
+++ MSP430FR5994/Examples/LEDs/inc/interrupts.h
@@ -0,0 +1,50 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interrupts header.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        19/September/2018
+ * @version     19/September/2018    The ORIGIN
+ * @warning     N/A
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+#include <msp430.h>
+
+#include "board.h"
+#include "variables.h"
+
+
+#ifndef INTERRUPTS_H_
+#define INTERRUPTS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**@brief Subroutine prototypes.
+ */
+
+
+
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+extern volatile uint32_t myState;
+extern volatile uint8_t  *myPtr;
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INTERRUPTS_H_ */
