2446d4cce8b9fc2061f02389ba8b34ef6a355082
Manuel Caballero <aqueronteblog@gmail.com>
2018-09-19 20:34:50 +0100

    MSP430FR5994 LEDs Example. Minor changes, all the necessary files were added: Functions/Interrupts/Board/Variables, they are still in progress

--- MSP430FR5994/Examples/LEDs/src/main.c
+++ MSP430FR5994/Examples/LEDs/src/main.c
@@ -1,5 +1,23 @@
+/**
+ * @brief       main.c
+ * @details     This example shows how to work with the external device: MAX44009. Every 2 seconds, a new
+ *              Lux value is gotten and transmitted through the UART ( Baud Rate: 115200 ).
+ *
+ *              The rest of the time, the microcontroller is in low power.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        14/September/2018
+ * @version     14/September/2018    The ORIGIN
+ * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0 ( SDK 1.1.0 ).
+ * @warning     Softdevice S310 was used although the file's name is S130. The softdevice is not used in this example anyway because of Bluetooth was not used.
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
 #include <msp430.h> 
-
+#include "board.h"
+#include "variables.h"
+#include "functions.h"
 
 /**
  * main.c
