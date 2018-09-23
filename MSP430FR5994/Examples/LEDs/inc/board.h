c9abd3d1cd287cb968a0a972cbb5b8a4b51245b2
Manuel Caballero <aqueronteblog@gmail.com>
2018-09-23 21:55:03 +0100

    MSP430FR5994 LEDs Example. LEDs were declared

--- MSP430FR5994/Examples/LEDs/inc/board.h
+++ MSP430FR5994/Examples/LEDs/inc/board.h
@@ -23,27 +23,10 @@ extern "C" {
 /**@brief LEDs.
  */
 typedef enum{
-  LED1  = 21,               /*!<   LED1: P0.21   */
-  LED2  = 22,               /*!<   LED2: P0.22   */
-  LED3  = 23,               /*!<   LED3: P0.23   */
-  LED4  = 24                /*!<   LED4: P0.24   */
-} NRF51DK_board_leds_t;
+  LED1  = 0x00,               /*!<   LED1: P1.0   */
+  LED2  = 0x01                /*!<   LED2: P1.1   */
+} EXP430FR5994_board_leds_t;
 
-
-/**@brief UART0.
- */
-typedef enum{
-  UART0_TX  =   9,          /*!<   UART TX: P0.9    */
-  UART0_RX  =   11          /*!<   UART RX: P0.11   */
-} NRF51DK_board_uart0_t;
-
-
-/**@brief TWI0.
- */
-typedef enum{
-  TWI0_SCL  =   7,          /*!<   TWI0 SCL: P0.7    */
-  TWI0_SDA  =   30          /*!<   TWI0 SDA: P0.30   */
-} NRF51DK_board_twi0_t;
 
 
 #ifdef __cplusplus
