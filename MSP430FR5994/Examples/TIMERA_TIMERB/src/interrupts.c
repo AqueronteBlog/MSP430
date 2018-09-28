ac9b426f87b3ce8242504c6884aa47eb759b6ae1
Manuel Caballero <aqueronteblog@gmail.com>
2018-09-28 20:03:40 +0100

    MSP430FR5994 Timer_A and Timer_B Example. Interrupts files were added

---
+++ MSP430FR5994/Examples/TIMERA_TIMERB/src/interrupts.c
@@ -0,0 +1,15 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interruption sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        19/September/2018
+ * @version     19/September/2018    The ORIGIN
+ * @warning     N/A
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+
+#include "interrupts.h"
+
