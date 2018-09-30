fe503938c7723a65d06ae01a846e45b8fda8b8e8
Manuel Caballero <aqueronteblog@gmail.com>
2018-09-30 21:06:36 +0100

    MSP430FR5994 Timer_A and Timer_B Example. TimerA and TimerB functions were defined

--- MSP430FR5994/Examples/TIMERA_TIMERB/inc/functions.h
+++ MSP430FR5994/Examples/TIMERA_TIMERB/inc/functions.h
@@ -5,8 +5,8 @@
  * @return      N/A
  *
  * @author      Manuel Caballero
- * @date        19/September/2018
- * @version     19/September/2018   The ORIGIN
+ * @date        28/September/2018
+ * @version     28/September/2018   The ORIGIN
  * @warning     N/A
  * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
  */
@@ -27,6 +27,8 @@ extern "C" {
  */
 void conf_WDT    ( void );
 void conf_GPIO   ( void );
+void conf_TimerA ( void );
+void conf_TimerB ( void );
 
 
 /**@brief Constants.
