05f3c01dc332e1d9d94afa2fb005e9c488df98e1
Manuel Caballero <aqueronteblog@gmail.com>
2018-09-30 21:07:58 +0100

    MSP430FR5994 Timer_A and Timer_B Example. Some comments were improved, TimerA and TimerB functions were placed, it is still in progress

--- MSP430FR5994/Examples/TIMERA_TIMERB/src/main.c
+++ MSP430FR5994/Examples/TIMERA_TIMERB/src/main.c
@@ -6,8 +6,8 @@
  * @return      N/A
  *
  * @author      Manuel Caballero
- * @date        14/September/2018
- * @version     14/September/2018    The ORIGIN
+ * @date        28/September/2018
+ * @version     28/September/2018    The ORIGIN
  * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0 ( SDK 1.1.0 ).
  * @warning     Softdevice S310 was used although the file's name is S130. The softdevice is not used in this example anyway because of Bluetooth was not used.
  * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
@@ -29,7 +29,6 @@
 
 /**@brief Function Prototypes.
  */
-void myDelay    ( uint16_t myCounter );
 
 
 
@@ -37,46 +36,18 @@ void myDelay    ( uint16_t myCounter );
  */
 int main(void)
 {
-    conf_WDT  ();
-    conf_GPIO ();
-
+    conf_WDT    ();
+    conf_GPIO   ();
+    conf_TimerA ();
+    conf_TimerB ();
 
 
     while ( 1 )
     {
         P1OUT   |=   LED1;      // LED 1 on
         P1OUT   &=  ~LED2;      // LED 2 off
-        myDelay ( 0x2323 );
 
         P1OUT   &=  ~LED1;      // LED 1 off
         P1OUT   |=   LED2;      // LED 2 on
-        myDelay ( 0x2323 );
     }
-}
-
-
-
-/**
- * @brief       void myDelay ( uint16_t )
- * @details     A simple delay.
- *
- *
- * @param[in]    myCounter: A simple counter.
- *
- * @param[out]   N/A.
- *
- *
- *@return      N/A
- *
- * @author      Manuel Caballero
- * @date        23/September/2018
- * @version     23/September/2018      The ORIGIN
- * @pre         N/A
- * @warning     N/A
- */
-void myDelay ( uint16_t myCounter )
-{
-    uint16_t i = 0;
-
-    for ( i = 0; i < myCounter; i++ );
 }
