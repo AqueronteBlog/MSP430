e865df00c4a1e8093a260ea6b29afd4e1350707d
Manuel Caballero <aqueronteblog@gmail.com>
2018-09-30 21:07:03 +0100

    MSP430FR5994 Timer_A and Timer_B Example. TimerA is almost ready, TimerB function is still in progress

--- MSP430FR5994/Examples/TIMERA_TIMERB/src/functions.c
+++ MSP430FR5994/Examples/TIMERA_TIMERB/src/functions.c
@@ -6,8 +6,8 @@
  * @return      N/A
  *
  * @author      Manuel Caballero
- * @date        19/September/2018
- * @version     19/September/2018   The ORIGIN
+ * @date        28/September/2018
+ * @version     28/September/2018   The ORIGIN
  * @warning     N/A
  * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
  */
@@ -28,8 +28,8 @@
  * @return      N/A
  *
  * @author      Manuel Caballero
- * @date        23/September/2018
- * @version     23/September/2018      The ORIGIN
+ * @date        28/September/2018
+ * @version     28/September/2018      The ORIGIN
  * @pre         N/A
  * @warning     N/A
  */
@@ -60,8 +60,8 @@ void conf_WDT  ( void )
  * @return      N/A
  *
  * @author      Manuel Caballero
- * @date        23/September/2018
- * @version     23/September/2018      The ORIGIN
+ * @date        28/September/2018
+ * @version     28/September/2018      The ORIGIN
  * @pre         N/A
  * @warning     N/A
  */
@@ -73,4 +73,87 @@ void conf_GPIO  ( void )
 
     P1OUT   &=  ~( LED1 | LED2 );
     P1DIR   |=   ( LED1 | LED2 );
+}
+
+
+
+/**
+ * @brief       void conf_TimerA  ( void )
+ * @details     It configures the TimerA.
+ *
+ *                  Timer A0:
+ *                      - Timer A0: Up mode
+ *                      - ACLK clock ( f_TA0/1 = ACLK = VLOCLK ~ 9.4KHz )
+ *                      - Interrupt enabled
+ *                      - Timer A0 overflow ~ 0.25s ( Overflow: 2350/9.4kHz = 0.25s )
+ *
+ *
+ * @param[in]    N/A.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        30/September/2018
+ * @version     30/September/2018      The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+void conf_TimerA  ( void )
+{
+    /* Timer TA0:
+     *  - Reset TimerA clock
+     *  - Reset input divider
+     *  - Stop TimerA0
+     *  - TimerA interrupt disabled
+     *  - Reset TimerA flag
+     */
+    TA0CTL  &=  ~( TASSEL | ID | MC | TAIE | TAIFG );
+
+    /* No capture mode   */
+    TA0CCTL0    &=  ~( CM );
+
+    /* Timer TA0:
+     *  - TimerA0 clock: ACLK
+     *  - Internal divider: /1
+     *  - TimerA0 interrupt enabled
+     */
+    TA0CTL  |=   ( TASSEL__ACLK | ID_0 | TAIE );
+    TA0EX0  &=  ~( TAIDEX );
+
+    /*  Capture/Compare register to generate a delay of about 0.25s   */
+    TA0CCR0  =   2350;
+
+    /* Timer TA0:
+     *  - TimerA0 mode: Up mode
+     *  - TimerA0 clear
+     */
+    TA0CTL  |=   ( MC__UP | TACLR );
+}
+
+
+
+/**
+ * @brief       void conf_TimerB  ( void )
+ * @details     It configures the TimerB.
+ *
+ *
+ * @param[in]    N/A.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        30/September/2018
+ * @version     30/September/2018      The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+void conf_TimerB  ( void )
+{
+
 }
