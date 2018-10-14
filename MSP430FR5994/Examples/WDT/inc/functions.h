fdf67c8628c7ad0c95c0986012090000c27c3dd3
Manuel Caballero <aqueronteblog@gmail.com>
2018-10-14 20:11:00 +0100

    MSP430FR5994 WDT Example. Functions files were added

---
+++ MSP430FR5994/Examples/WDT/inc/functions.h
@@ -0,0 +1,47 @@
+/**
+ * @brief       functions.h
+ * @details     Functions's header.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        14/October/2018
+ * @version     14/October/2018   The ORIGIN
+ * @warning     N/A
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+#include <msp430.h>
+
+#include "board.h"
+#include "variables.h"
+
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**@brief Function prototypes.
+ */
+void conf_WDT    ( void );
+void conf_CLK    ( void );
+void conf_GPIO   ( void );
+
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FUNCTIONS_H_ */
