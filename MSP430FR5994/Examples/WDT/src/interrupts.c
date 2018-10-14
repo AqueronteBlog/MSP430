82b7ae3cd438e61c9ee77abae030e488af475810
Manuel Caballero <aqueronteblog@gmail.com>
2018-10-14 20:11:14 +0100

    MSP430FR5994 WDT Example. Interrupts files were added

---
+++ MSP430FR5994/Examples/WDT/src/interrupts.c
@@ -0,0 +1,39 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interruption sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        7/October/2018
+ * @version     7/October/2018    The ORIGIN
+ * @warning     N/A
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+
+#include "interrupts.h"
+
+
+/**
+ * @brief       WDT_A_ISR interrupt service routine
+ * @details     Subroutine for WDT_A.
+ *
+ *
+ * @param[in]    N/A.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        14/October/2018
+ * @version     14/October/2018      The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#pragma vector = WDT_VECTOR
+__interrupt void WDT_A_ISR ( void )
+{
+    P1OUT   ^=   ( LED1 | LED2 );              // Change LED1 and LED2 state
+}
