178005c50d98ea39655a15db2f13b76c86c676d5
Manuel Caballero <aqueronteblog@gmail.com>
2018-10-31 19:58:19 +0000

    MSP430FR5994 GPIO_IN Example. Interrupts files were added

---
+++ MSP430FR5994/Examples/GPIO_IN/inc/interrupts.h
@@ -0,0 +1,48 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interrupts header.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        31/October/2018
+ * @version     31/October/2018    The ORIGIN
+ * @warning     N/A
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+#include <msp430.h>
+
+#include "board.h"
+#include "variables.h"
+
+
+#ifndef INTERRUPTS_H_
+#define INTERRUPTS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**@brief Subroutine prototypes.
+ */
+__interrupt void WDT_A_ISR ( void );
+
+
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INTERRUPTS_H_ */
