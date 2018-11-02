f3d58d3abfd9b7caac914284122c2b0d2bc03bd1
Manuel Caballero <aqueronteblog@gmail.com>
2018-11-02 20:27:09 +0000

    MSP430FR5994 GPIO_IN Example. Button pins were configured with interrupt enabled

--- MSP430FR5994/Examples/GPIO_IN/src/functions.c
+++ MSP430FR5994/Examples/GPIO_IN/src/functions.c
@@ -53,6 +53,10 @@ void conf_WDT  ( void )
  *                      -- P1.0:    LED1
  *                      -- P1.1:    LED2
  *
+ *                  - PORT5: Interrupt enabled
+ *                      -- P5.5:    S2  ( BUTTON1 )
+ *                      -- P5.6:    S1  ( BUTTON2 )
+ *
  * @param[in]    N/A.
  *
  * @param[out]   N/A.
@@ -62,17 +66,32 @@ void conf_WDT  ( void )
  *
  * @author      Manuel Caballero
  * @date        31/October/2018
- * @version     31/October/2018   The ORIGIN
+ * @version     1/November/2018   Buttons were added
+ *              31/October/2018   The ORIGIN
  * @pre         N/A
  * @warning     N/A
  */
 void conf_GPIO  ( void )
 {
+    /* Configure the Buttons on PORT5  */
+    P5SEL0  &=  ~( S1 | S2 );
+    P5SEL1  &=  ~( S1 | S2 );
+
+    /* Input with pull-up resistors  */
+    P5REN   |=   ( S1 | S2 );
+    P5OUT   |=   ( S1 | S2 );
+
+    P5DIR   &=  ~( S1 | S2 );
+
+    /* Enable interrupts on P5.5 and P5.6 pins     */
+    P5IES   |=   ( S1 | S2 );   // P5IFG  flag is set with a high-to-low transition
+    P5IFG   &=  ~( S1 | S2 );   // Reset flags
+    P5IE    |=   ( S1 | S2 );   // Enable interrupts on both pins
+
     /* Configure the LEDs ( output low ) on PORT1   */
     P1SEL0  &=  ~( LED1 | LED2 );
     P1SEL1  &=  ~( LED1 | LED2 );
 
-    P1OUT   &=  ~( LED1 );
-    P1OUT   |=   LED2;
+    P1OUT   &=  ~( LED1 | LED2 );
     P1DIR   |=   ( LED1 | LED2 );
 }
