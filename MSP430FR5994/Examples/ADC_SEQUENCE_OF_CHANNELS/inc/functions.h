842302dd8caf77e001bf40fa21562481b992d6b9
Manuel Caballero <aqueronteblog@gmail.com>
2019-05-15 20:00:00 +0100

    MSP430FR5994  ADC Sequence of Channels Example. Functions files were added

---
+++ MSP430FR5994/Examples/ADC_SEQUENCE_OF_CHANNELS/inc/functions.h
@@ -0,0 +1,51 @@
+/**
+ * @brief       functions.h
+ * @details     Functions's header.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        15/May/2019
+ * @version     15/May/2019   The ORIGIN
+ * @warning     N/A
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+#include <msp430.h>
+
+#include "board.h"
+#include "variables.h"
+
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**@brief Function prototypes.
+ */
+void conf_WDT    ( void );
+void conf_CLK    ( void );
+void conf_GPIO   ( void );
+void conf_UART   ( void );
+void conf_ADC    ( void );
+void conf_REF_A  ( void );
+void conf_TimerB ( void );
+
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FUNCTIONS_H_ */
