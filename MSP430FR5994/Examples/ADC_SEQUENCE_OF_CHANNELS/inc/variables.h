961d112a709d6ecf55b0ba6cebce6204a5276318
Manuel Caballero <aqueronteblog@gmail.com>
2019-05-15 20:00:24 +0100

    MSP430FR5994  ADC Sequence of Channels Example. Variables file was added

---
+++ MSP430FR5994/Examples/ADC_SEQUENCE_OF_CHANNELS/inc/variables.h
@@ -0,0 +1,44 @@
+/**
+ * @brief       variables.h
+ * @details     Constants and variables used by the example.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        15/May/2019
+ * @version     15/May/2019     The ORIGIN
+ * @warning     N/A
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+#include <stdint.h>
+
+#ifndef VARIABLES_H_
+#define VARIABLES_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**@brief Constants.
+ */
+
+
+/**@brief States of the system.
+ */
+typedef enum{
+  STATE_LOW_POWER     = BIT0,   /*!<   Low power state          */
+  PACK_TEMPERATURE    = BIT1,   /*!<   Process the temperature  */
+  SEND_TEMP_OVER_UART = BIT2    /*!<   Send data over UART      */
+} system_states_t;
+
+/**@brief Variables.
+ */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VARIABLES_H_ */
